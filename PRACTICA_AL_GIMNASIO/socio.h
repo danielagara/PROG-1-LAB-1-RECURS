08cf017de29f7114199562649353b7178aeba299
danielagara <daniela_mariel_garat@hotmail.com>
2017-11-02 22:11:54 -0300

    arraylist y practica gimnasio mil archivos

---
+++ PRACTICA_AL_GIMNASIO/socio.h
@@ -0,0 +1,14 @@
+#ifndef SOCIO_H_INCLUDED
+#define SOCIO_H_INCLUDED
+
+typedef struct
+{
+    int idSocio;
+    char nombreSocio[51];
+    char apellidoSocio[51];
+    char DNI[51];
+    int flagDeEstado;
+}ESocio;
+
+
+#endif // SOCIO_H_INCLUDED
